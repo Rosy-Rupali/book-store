fb53b436a18f1bc7376e8e79b455befc1236ec12
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-10-05 03:20:28 +0530

    added: the wishlist component and called the api for it

--- src/components/WishList.js
+++ src/components/WishList.js
@@ -2,41 +2,55 @@ import React, { useState, useEffect } from "react";
 import Footer from "./Footer";
 import HomePageHeader from "./HomePageHeader";
 import bookImage from "../assets/bookImage.png";
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import Divider from "@mui/material/Divider";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import { useHistory } from "react-router";
 import "../css/WishList.css";
-import { getFromWishList, removeFromWishlist } from "../Services/BookService";
+import {
+  getFromWishList,
+  removeFromWishlist,
+  addCart,
+} from "../Services/BookService";
 
 const WishList = () => {
   const history = useHistory();
   const [wishList, setwishList] = useState([]);
 
   const getWishListItems = () => {
-      getFromWishList()
+    getFromWishList()
       .then((response) => {
-          console.log(response);
-          setwishList(response.data.result);
+        console.log(response);
+        setwishList(response.data.result);
       })
       .catch((error) => {
-          console.log(error)
-      })
-  }
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
-      getWishListItems();
-  }, [])
+    getWishListItems();
+  }, []);
+  const addToCart = (productid) => {
+    addCart(productid)
+      .then((response) => {
+        console.log(response);
+        handleRemove(productid);
+        history.push("/cart");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const handleRemove = (productid) => {
-      removeFromWishlist(productid)
+    removeFromWishlist(productid)
       .then((response) => {
-          console.log(response);
-          getWishListItems();
+        console.log(response, "remove from wishlist");
+        getWishListItems();
       })
       .catch((error) => {
-          console.log(error)
-      })
-  }
+        console.log(error);
+      });
+  };
 
   return (
     <div>
@@ -48,34 +62,31 @@ const WishList = () => {
       <div className="wish-container">
         <div className="wishlistitem-container">
           <h3 className="wish-head">My WishList({wishList.length})</h3>
-          {wishList.map((book) => ( 
-          <div className="cart-image2">
-            <div className="wishimg-details">
-              <img className="image2" src={bookImage} alt="book" />
-              <div className="details-cart">
-                <h3 className="head-tagname">{book.product_id.bookName}</h3>
-                <p className="head-tag-para">by {book.product_id.author}</p>
-                <h5 className="head-tag">Rs {book.product_id.price}</h5>
+          {wishList.map((book) => (
+            <div className="cart-image2">
+              <div className="wishimg-details">
+                <img className="image2" src={bookImage} alt="book" />
+                <div className="details-cart">
+                  <h3 className="head-tagname">{book.product_id.bookName}</h3>
+                  <p className="head-tag-para">by {book.product_id.author}</p>
+                  <h5 className="head-tag">Rs {book.product_id.price}</h5>
+                </div>
+              </div>
+
+              <div className="add-del">
+                <div
+                  className="addbag1"
+                  onClick={() => addToCart(book.product_id._id)}
+                >
+                  Add To Bag
+                </div>
+                <DeleteForeverIcon
+                  className="del-icon"
+                  style={{ size: "small", color: "#ccc" }}
+                  onClick={() => handleRemove(book.product_id._id)}
+                />
               </div>
             </div>
-            
-            <div className="add-del">
-              <div
-                className="addbag1"
-               // onClick={() => addToCart(book.product_id._id)}
-              >
-                Add To Bag
-              </div>
-              <DeleteForeverIcon
-                className="del-icon"
-                style={{ size: "small", color: "#ccc" }}
-                onClick={() => handleRemove(book.product_id._id)}
-              />
-              
-            </div>
-           
-          </div>
-         
           ))}
         </div>
       </div>
